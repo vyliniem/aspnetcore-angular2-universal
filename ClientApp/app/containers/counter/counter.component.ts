87c1423eca46a30ccae06fac702fba6d4ded9319
Ville Yliniemi <vyliniem@gmail.com>
2017-11-17 15:35:41 +0200

    Demonstration of timer issue with prerender

--- ClientApp/app/containers/counter/counter.component.ts
+++ ClientApp/app/containers/counter/counter.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs/Rx';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 
 @Component({
     selector: 'app-counter',
     templateUrl: './counter.component.html'
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit, OnDestroy {
     public currentCount = 0;
+    public ticks: number;
+
+    private timer: Observable<any>;
+    private timerSub: Subscription;
+
+    constructor(
+        @Inject(PLATFORM_ID) private platformId: Object
+    ) {}
+
+    ngOnInit() {
+        if (isPlatformBrowser(this.platformId)) {
+            this.timerSub = new Subscription();
+            this.startTimer(10);
+        }
+    }
+
+    ngOnDestroy() {
+        if (isPlatformBrowser(this.platformId)) {
+            this.timerSub.unsubscribe();
+        }
+    }
 
     public incrementCounter() {
         this.currentCount++;
+    }
+
+    private startTimer(seconds: number = 10) {
+        this.ticks = seconds;
+        this.timer = Observable.timer(1000, 1000)
+            .timeInterval()
+            .pluck('value')
+            .take(seconds);
+        this.timerSub = this.timer.subscribe(t => {
+            console.log('Timer', t);
+            if (t >= seconds - 1) {
+                this.incrementCounter();
+                this.startTimer(seconds);
+            } else {
+                this.ticks--;
+            }
+        }, (err) => console.error('Timer error', err));
     }
 }
